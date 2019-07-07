4e132f07e3561e7009ff15a88132e455e91bb0c2
JIEWU <vjiewu@gmail.com>
2019-07-07 19:40:21 +0800

    Update main.cpp

--- Server/main.cpp
+++ Server/main.cpp
@@ -1,11 +1,13 @@
 #include<QApplication>
 #include <QtCore/QTextCodec>
 #include"server_message.h"
+#inlucde"fileserver.h"
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     QTextCodec::setCodecForLocale(QTextCodec::codecForName("utf-8"));
     ServerMessage w;
+    fileserver v;
     w.runServer();
     return a.exec();
 }
