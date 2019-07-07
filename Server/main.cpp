7a347d5a0048dde9d35eed266dc7bbaaef1ec1ef
JIEWU <vjiewu@gmail.com>
2019-07-07 19:40:48 +0800

    Update main.cpp

--- Server/main.cpp
+++ Server/main.cpp
@@ -1,7 +1,7 @@
 #include<QApplication>
 #include <QtCore/QTextCodec>
 #include"server_message.h"
-#inlucde"fileserver.h"
+#include"fileserver.h"
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
